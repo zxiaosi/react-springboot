574bf85f004488598b86dfc2d20f02b1edf5ebff
zxiaosi <2692159310@qq.com>
2023-07-25 13:56:30 +0800

    初始化仓库

---
+++ backend/weapp/src/main/java/com/zxiaosi/weapp/WeappApplication.java
@@ -0,0 +1,13 @@
+package com.zxiaosi.weapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeappApplication.class, args);
+    }
+
+}
---
+++ backend/weapp/src/test/java/com/zxiaosi/weapp/WeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxiaosi.weapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
